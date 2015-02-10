277f31224a29ad20bbf7b3efb8fccdf9f56c3d58
jonyB0B <sp.jona@gmail.com>
2015-02-10 21:29:02 +0100

    Delete 5-5-12.js

--- 5-5-12.js
+++
@@ -1,7 +0,0 @@
-function rellenarForm(){
-  salida.style.backgroundColor = form.value;
-}
-
-var salida = document.getElementById("salida");
-var form = document.getElementById("formulario");
-form.addEventListener("input", rellenarForm);
